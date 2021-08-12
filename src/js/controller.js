da82485c5df8c7f4805c4ddb115fae1d946fec0b
strongcurrentscf <cfrancis1989@gmail.com>
2021-08-11 22:09:27 -0400

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -360,6 +360,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
