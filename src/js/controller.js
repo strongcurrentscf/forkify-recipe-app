1417b34ae8b7e3fe06c3eee67b33d86cf5741a5b
strongcurrentscf <cfrancis1989@gmail.com>
2022-05-04 18:38:06 -0400

    Responsiveness added

--- src/js/controller.js
+++ src/js/controller.js
@@ -360,6 +360,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -368,6 +372,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature(); // just a test add
 };
 init();
 
